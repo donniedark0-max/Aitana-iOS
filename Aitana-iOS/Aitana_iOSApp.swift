135c33aaaa30602ee296f0ff8e799da635a0af12
donniedark0-max <juan.vilca18@outlook.com>
2025-11-07 18:38:05 -0500

    Initial Commit

---
+++ Aitana-iOS/Aitana_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aitana_iOSApp.swift
+//  Aitana-iOS
+//
+//  Created by Juan Vilca on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Aitana_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aitana-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aitana-iOS
+//
+//  Created by Juan Vilca on 11/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, im AItana!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
